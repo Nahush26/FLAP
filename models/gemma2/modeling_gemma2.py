7cd8743634add36c8aa866b0460c68fb87e84244
Shwu <shubham.kushwaha@nyunai.com>
2024-09-10 09:50:49 +0000

    gemma2 mlp mixin

--- models/gemma2/modeling_gemma2.py
+++ models/gemma2/modeling_gemma2.py
@@ -49,7 +49,7 @@ from transformers.utils import (
     replace_return_docstrings,
 )
 from .configuration_gemma2 import Gemma2Config
-
+from .._model_mixins import BaseMLP
 
 if is_flash_attn_2_available():
     from transformers.modeling_flash_attention_utils import _flash_attention_forward
@@ -215,15 +215,24 @@ def apply_rotary_pos_emb(q, k, cos, sin, position_ids=None, unsqueeze_dim=1):
     return q_embed, k_embed
 
 
-class Gemma2MLP(nn.Module):
-    def __init__(self, config):
-        super().__init__()
+class Gemma2MLP(BaseMLP):
+    def __init__(self, config: Gemma2Config, layer_idx: int):
+        super().__init__(config=config, layer_idx=layer_idx)
         self.config = config
         self.hidden_size = config.hidden_size
         self.intermediate_size = config.intermediate_size
-        self.gate_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
-        self.up_proj = nn.Linear(self.hidden_size, self.intermediate_size, bias=False)
-        self.down_proj = nn.Linear(self.intermediate_size, self.hidden_size, bias=False)
+        self.__patch_mlp_init()
+        self.gate_proj = nn.Linear(
+            self.hidden_size, self.intermediate_size, bias=config.mlp_bias
+        )
+        self.up_proj = nn.Linear(
+            self.hidden_size, self.intermediate_size, bias=config.mlp_bias
+        )
+        self.down_proj = nn.Linear(
+            self.intermediate_size,
+            self.hidden_size,
+            bias=config.mlp_bias or layer_idx >= config.first_pruned_layer_idx,
+        )
         self.act_fn = ACT2FN[config.hidden_activation]
 
     def forward(self, x):
@@ -628,7 +637,7 @@ class Gemma2DecoderLayer(nn.Module):
             config=config, layer_idx=layer_idx
         )
 
-        self.mlp = Gemma2MLP(config)
+        self.mlp = Gemma2MLP(config, layer_idx=layer_idx)
         self.input_layernorm = Gemma2RMSNorm(
             config.hidden_size, eps=config.rms_norm_eps
         )
